c4b0d400339ef1cd4acc0b6093760114a78ce876
jueun-k1m <jueunkim.ee@gmail.com>
2025-12-05 09:29:29 +0900

    Rename function to save_final_data and update logic

--- omnitor/omnitor/services/save_finaldata.py
+++ omnitor/omnitor/services/save_finaldata.py
@@ -1,12 +1,31 @@
-from models import CalibrationSettings, FinalData
+import os
+import sys
+import math
+from datetime import datetime
+import django
+from omnitor.models import CalibrationSettings, FinalData, RawData
 
-def calc_and_save_final_data(filtered_data):
+def save_final_data(filtered_data):
     """
-    필터링 된 데이터에 보정 설정을 적용하여 FInalData DB에 최종 데이터를 저장
+    필터링 된 데이터에 보정 설정을 적용하여 FinalData DB에 최종 데이터를 저장
     """
 
     if not filtered_data:
         return None
-    
-    settings = CalibrationSettings.load()
-    
+        
+
+    FinalData.objects.create(
+        timestamp=RawData.timestamp,
+        air_temperature=filtered_data['air_temperature'],
+        air_humidity=filtered_data['air_humidity'],
+        co2=filtered_data['co2'],
+        insolation=filtered_data['insolation'],
+        weight=CalibrationSettings.weight_slope * filtered_data['weight'] + CalibrationSettings.weight_intercept,
+        water_temperature=filtered_data['water_temperature'],
+        ph=(CalibrationSettings.ph_slope * filtered_data['ph']) + CalibrationSettings.ph_intercept,
+        ec=(CalibrationSettings.ec_slope * filtered_data['ec']) + CalibrationSettings.ec_intercept,
+        soil_temperature=filtered_data['soil_temperature'],
+        soil_humidity=filtered_data['soil_humidity'],
+        soil_ec=filtered_data['soil_ec'],
+        soil_ph=filtered_data['soil_ph']
+    )
