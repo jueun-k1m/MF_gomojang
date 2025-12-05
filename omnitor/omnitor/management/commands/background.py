104a166e77f66eb6c1f1af6b00febb003bd5e25d
jueun-k1m <jueunkim.ee@gmail.com>
2025-12-05 09:31:38 +0900

    Add background command for FarmJournal management

---
+++ omnitor/omnitor/management/commands/background.py
@@ -0,0 +1,15 @@
+# 백그라운드로 실행할 것들
+
+
+
+from models import FarmJournal
+import sched
+
+save_rawdata.py
+
+filtered data = filter.py
+
+save_finaldata.py(filtered_data)
+
+cam.py schedule for "FarmJournal.cam_time"
+
